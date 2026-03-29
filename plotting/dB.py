5713ee59e30cd424a37639c3fcf5641c7a99a6d3
root <root@PCV2.localdomain>
2026-03-29 17:44:16 -0600

    Updated db plotting script

--- plotting/dB.py
+++ plotting/dB.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dB = np.loadtxt("dB.txt")
-
-n_splines =  30
-
+metadata = np.loadtxt("dB_meta.txt")
+n_splines = int(metadata[0])
+x = metadata[1:]
+n_x = len(x)
 
 real = dB[:, 0].reshape(n_splines, n_x)
 imag = dB[:, 1].reshape(n_splines, n_x)
