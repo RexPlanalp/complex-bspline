ab19fa0faf9eb14c215221bba0525c6f1dd3c01c
root <root@PCV2.localdomain>
2026-03-29 21:24:19 -0600

    Swapped new for from in some places

--- src/bspline.rs
+++ src/bspline.rs
@@ -68,17 +68,17 @@ impl BSpline {
     fn b_recursive(&self, i: usize, x: Complex64, degree: usize) -> Complex64 {
         if degree == 0 {
             return if self.knot_vector.in_interval(x.re, i) {
-                Complex64::new(1.0, 0.0)
+                Complex64::from(1.0)
             } else {
-                Complex64::new(0.0, 0.0)
+                Complex64::from(0.0)
             };
         }
 
         let denom1 = self.knot_vector[i + degree] - self.knot_vector[i];
         let denom2 = self.knot_vector[i + degree + 1] - self.knot_vector[i + 1];
 
-        let mut term1 = Complex64::new(0.0, 0.0);
-        let mut term2 = Complex64::new(0.0, 0.0);
+        let mut term1 = Complex64::from(0.0);
+        let mut term2 = Complex64::from(0.0);
 
         if denom1.abs() != 0.0 {
             term1 = (x - self.knot_vector[i]) / (denom1) * self.b_recursive(i, x, degree - 1);
